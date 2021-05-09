5bef7b899669f7bc3533de9dfa2dfaee4485f55c
me-am-i <vsl_sv@bk.ru>
2021-05-09 23:01:03 +0300

    refactor: изменение стиля и переименование

--- caesar-cipher-cli/src/readFile.js
+++ caesar-cipher-cli/src/readFile.js
@@ -5,10 +5,10 @@ const args = require('./input');
 const fileName = path.normalize(args.input);
 
 module.exports = async () => {
-  try {
-  return await fs.readFile(fileName, 'utf8');
-  } catch (error) {
-   console.error("error: File doesn't exist or not accessible, please ensure that path is correct", error.path) // from creation or business logic
-   process.exit(1);
+    try {
+        return await fs.readFile(fileName, 'utf8');
+    } catch (error) {
+        console.error("error: File doesn't exist or not accessible, please ensure that path is correct", error.path) // from creation or business logic
+        process.exit(1);
   }
 };
