d87cfabc3281439e7006cc88818173368f6d2044
me-am-i <vsl_sv@bk.ru>
2021-05-09 22:58:33 +0300

    feat: добавлена запись в файл и обработка ошибок

---
+++ caesar-cipher-cli/src/writefile.js
@@ -0,0 +1,23 @@
+const fs = require('fs').promises;
+const path = require('path');
+const args = require('./input');
+
+const fileName = path.normalize(args.output);
+
+// module.exports = async () => {
+//     const asyncFunc = async () => await fs.appendFile('output.txt', 'demo text', 'utf8');
+//     //asyncFunc().then(console.log)
+//     return await asyncFunc();
+// };
+
+const toWrite = async (content) => {
+    try {
+        return await fs.appendFile(fileName, content + "\r\n");
+    }
+    catch (error) {
+        console.error("error: File can't be write, please ensure that path is correct and filesystem is writable", error.path)
+        process.exit(1);
+    }
+};
+
+module.exports = toWrite;
